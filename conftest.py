e0dd5208b50e98efe9c4ecf5eb1e9fd41fd15f96
Tashunya <tashunya_17@mail.ru>
2021-03-10 10:57:41 +0300

    change help text for language param

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: chrome or firefox")
+                     help="Choose language")
 
 
 @pytest.fixture(scope="function")
